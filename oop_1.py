b788e8f41e5be35d852c523f09df3fd288588004
Robert <rfgraves86@gmail.com>
2022-11-09 19:44:22 -0700

    additional work with OOP & class methods

---
+++ oop_1.py
@@ -0,0 +1,42 @@
+# class Dog:
+#     def __init__(self, name, breed, weight, age):
+#         self.name = name
+#         self.breed = breed
+#         self.weight = weight
+#         self.age = age
+
+#     def name_change(self, input):
+#         self.name = input
+#         return self
+
+#     def birthday(self):
+#         self.age += 1
+#         return self
+
+#     def introduction(self):
+#         return f"Hello, my name is {self.name} and I am {self.age}. Welcome to the neighborhood!"
+# dog1 = Dog("Pal", "Doodle", 5, 10)
+# print(dog1.introduction())
+
+
+# class Clown:
+#     def __init__(self, name, specialty, is_sad):
+#         self.name = name
+#         self.specialty = specialty
+#         self.is_sad = is_sad
+#         self.years_exp = 0
+
+#     def juggle(self, numb_balls):
+#         print(f"{self.name} is juggling {numb_balls} balls at one time!")    
+#         return self
+
+#     def anniversary(self):
+#         self.years_exp += 1
+#         return self
+
+#     def fav_trick(self, trick_type):
+#         print(f"{self.name}'s favorite trick is {trick_type}")
+#         return self
+# instance_name = Constructor(parameters of def__init__ fuction)
+# print(instance_name.__dict__) > Will print all attributes of the instance calling the __dict__ built-in.
+
