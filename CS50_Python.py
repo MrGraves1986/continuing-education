e1662e68f463154d0134d34b6ff5cce934007483
Robert <rfgraves86@gmail.com>
2022-12-14 11:05:43 -0700

    Further class work & decorator

--- CS50_Python.py
+++ CS50_Python.py
@@ -79,6 +79,91 @@
 
 # if not > This can be used in a python function. It means,"If (condition) == 0...". 
 
-if not self.function():
-    return False
+# if not self.function():
+#     return False
 #If the function output equals 0, return 'False' 
+
+# def square(x):
+#     return x*x
+
+# for i in range(10):
+#     print(f"The squre of {i} is {square(i)}!")    
+
+# x = int(input("Number: "))
+
+# print(f"The number you chose is {x}")
+
+# num = int(input("Number: "))
+
+# if num > 0:
+#     print("Number is positive.")
+# elif num < 0:
+#     print("Number is actually negative.")
+# else:
+#     print("I have no clue but think it might be 0.")
+
+# dog = {
+#     "breed": "Lab",
+#     "name": "Chaco",
+#     "fav_toy": "Ball"
+# }
+
+# print(dog["breed"])
+
+# names = ["John", "Larry", "Bill"]
+
+# for name in names:
+#     print(name)
+
+# Modules: used to help in bigger projects by having functions in one file and running them in another. For example, if I have a module named functions.py and loops.py, I would have to import the functions.py folder into loops.py via 'import functions' at top of my document. Once imported, and I wanted to call a function, I would use .notation >
+
+
+# functions.py
+
+# def mult(x)
+#    return x*x
+
+# *******************
+# loops.py
+
+# import functions
+# 
+# for i range(5)
+    # print(f"The square of {i} is {functions.mult(i)}")
+
+# class Point():
+#     def __init__(self, x, y):
+#         self.x = x
+#         self.y = y
+
+# point1 = Point(3,5)
+
+# print(point1.y)
+
+# class Flight():
+#     def __init__(self, capacity):
+#         self.capacity = capacity
+#         self.passengers = []
+
+#     def add_passenger(self, passenger):
+#         if not self.open_seats():
+#             return False
+#         self.passengers.append(passenger)
+#         return True
+
+#     def open_seats(self):
+#         return self.capacity - len(self.passengers)
+
+# def announce(f):
+#     def wrapper():
+#         print("About to run the function")
+#         f()
+#         print("Done with function")
+
+#     return wrapper
+
+# @announce
+# def hello():
+#     print("Hello, world!")
+
+# hello()    
